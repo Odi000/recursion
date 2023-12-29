973caebe5069cbefc8e10478fb38c04065865438
Odi000 <mandimandi.ro@gmail.com>
2023-12-29 03:44:17 +0100

    LinkedList completed

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ body.appendChild(div_0);
 
 
 function reverse(string) {
-    if(!string.length) return;
+    if (!string.length) return;
     result_0.textContent = '';
     let reversedStr = '';
 
@@ -128,13 +128,99 @@ function reverse(string) {
         reversedStr = string[i] + reversedStr;
     }
 
-    letterByLetter(reversedStr);    
+    letterByLetter(reversedStr);
 }
 
 function letterByLetter(string) {
     result_0.textContent += string[0];
-    if(string.length>1) {
+    if (string.length > 1) {
         string = string.slice(1);
-        setTimeout(()=>letterByLetter(string),65);
+        setTimeout(() => letterByLetter(string), 65);
     }
 }
+
+
+// Linked List Assignment ---*---*----*---*----*---*---
+
+class Node {
+    constructor(value) {
+        this.value = null;
+        this.pointer = null;
+    }
+}
+
+class LinkedList {
+    counter = 0;
+
+    append(value) {
+        const newNode = new Node();
+
+        newNode.value = value;
+        if (this.counter >= 1) this[this.counter - 1].pointer = newNode;
+        this[this.counter++] = newNode;
+    }
+
+    prepend(value) {
+        const newNode = new Node();
+
+        newNode.value = value;
+        if (this.counter == 0) {
+            return this[this.counter++] = newNode;
+        }
+        for (let i = this.counter++; i >= 0; i--) {
+            if (i === 0) {
+                this[i] = newNode;
+                newNode.pointer = this[i + 1];
+            }
+            else {
+                this[i] = this[i - 1];
+            }
+        }
+    }
+
+    get size() {
+        return this.counter;
+    }
+
+    get head() {
+        return this[0];
+    }
+
+    get tail() {
+        return this[this.counter - 1];
+    }
+
+    at(index) {
+        return this[index];
+    }
+
+    pop() {
+        delete this[this.counter - 1];
+        this[this.counter - 2].pointer = null;
+        this.counter--;
+    }
+
+    contains(value) {
+        let result = false;
+        for (let i = 0; i < this.counter; i++) {
+            if (value === this[i].value) result = true;
+        } return result;
+    }
+
+    find(value) {
+        for (let i = 0; i < this.counter; i++) {
+            if (value === this[i].value) return i;
+        } return null;
+    }
+}
+
+const vGTAje = new LinkedList();
+console.log(vGTAje);
+vGTAje.append("Real GTA 3");
+vGTAje.append("Vice City");
+vGTAje.append("San Andreas");
+console.log(vGTAje);
+vGTAje.prepend("GTA 1");
+console.log(vGTAje);
+vGTAje.append("GTA IV");
+vGTAje.append("GTA V");
